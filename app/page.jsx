5dd758ca10909eaac915f0a8aca9d5c88f7df964
Ismail Hossain <mdismailtanvir6@gmail.com>
2026-05-31 21:16:44 +0600

    initial commit

---
+++ app/page.jsx
@@ -0,0 +1,11 @@
+const Home = async () => {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black dark:text-white sm:items-start">
+        hello world !
+      </main>
+    </div>
+  );
+};
+
+export default Home;
