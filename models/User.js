66809039c833ec74596e5fb7c02b1d87c238e325
Ismail Hossain <mdismailtanvir6@gmail.com>
2026-06-03 22:55:08 +0600

    implement super admin setup and seeding

--- models/User.js
+++ models/User.js
@@ -1,4 +1,4 @@
-// models/User
+// models/User.js
 
 import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
@@ -11,6 +11,7 @@ const userSchema = new mongoose.Schema(
       trim: true,
       maxlength: [100, "Name cannot exceed 100 characters"],
     },
+
     email: {
       type: String,
       required: [true, "Email is required"],
@@ -19,27 +20,33 @@ const userSchema = new mongoose.Schema(
       trim: true,
       match: [/^\S+@\S+\.\S+$/, "Please provide a valid email"],
     },
+
     password: {
       type: String,
       required: [true, "Password is required"],
       minlength: [6, "Password must be at least 6 characters"],
       select: false,
     },
+
     role: {
       type: String,
-      enum: ["admin", "editor", "author"],
-      default: "author",
+      enum: ["super_admin", "moderator", "author", "user"],
+      default: "user",
     },
+
     avatar: {
       type: String,
       default: "",
     },
+
     isActive: {
       type: Boolean,
       default: true,
     },
   },
-  { timestamps: true },
+  {
+    timestamps: true,
+  },
 );
 
 // Hash password before saving
@@ -49,11 +56,11 @@ userSchema.pre("save", async function () {
   this.password = await bcrypt.hash(this.password, 12);
 });
 
-
 // Compare password method
-userSchema.methods.comparePassword = function (candidatePassword) {
+userSchema.methods.comparePassword = async function (candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
 
 const User = mongoose.models.User || mongoose.model("User", userSchema);
+
 export default User;
