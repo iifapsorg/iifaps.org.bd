e9d131e3a29522d05560bf8d97ebd636f31b63bb
Ismail Hossain <mdismailtanvir6@gmail.com>
2026-07-06 00:31:49 +0600

    refactor: improve blog pagination loading experience

--- app/(public)/blogs/page.jsx
+++ app/(public)/blogs/page.jsx
@@ -4,7 +4,7 @@ import { getBlogs } from "@/services/blog.service";
 import BlogCommonLayout from "@/components/blog/BlogCommonLayout";
 import Container from "@/components/shared/Container";
 import { generateMetaData } from "@/utils/generateMetaData";
-import Link from "next/link";
+import Pagination from "@/components/blog/Pagination";
 
 export const metadata = generateMetaData({
   title: "Blog",
@@ -43,45 +43,23 @@ export default async function BlogPage({ searchParams }) {
   // ======== getting articles by type =====
   const { blogs: allArticles, pages, total } = await getBlogs(filters);
 
-  // const {
-  //   blogs: allArticles,
-  //   pages,
-  //   total,
-  // } = await getBlogs({
-  //   page: currentPage,
-  //   limit: ARTICLE_LIMIT,
-  //   sortBy: type === "latest" ? "createdAt" : "views",
-  // });
-
   return (
     <main>
       <Container className="py-12">
+        {/* ====== blog ui ==== */}
         <BlogCommonLayout
           blogs={allArticles}
-          // sectionHeading={`${type?.replace(/-/g, " ") || "all"} articles : ${total}`}
           sectionHeading={`${currentType.replace(/-/g, " ")} articles : ${total}`}
           limit={ARTICLE_LIMIT}
           isBtn={false}
         />
 
         {/* Pagination */}
-        {pages > 1 && (
-          <div className="mt-10 flex justify-center gap-2">
-            {Array.from({ length: pages }, (_, i) => i + 1).map((p) => (
-              <Link
-                key={p}
-                href={`/blogs?type=${currentType}&page=${p}`}
-                className={`px-4 py-2 rounded-lg text-sm font-medium border transition-colors ${
-                  p === currentPage
-                    ? "bg-blue-600 text-white border-blue-600"
-                    : "border-gray-300 text-gray-600 hover:border-blue-400"
-                }`}
-              >
-                {p}
-              </Link>
-            ))}
-          </div>
-        )}
+        <Pagination
+          pages={pages}
+          currentPage={currentPage}
+          currentType={currentType}
+        />
       </Container>
     </main>
   );
---
+++ components/blog/Pagination.jsx
@@ -0,0 +1,53 @@
+// components/blog/pagination
+
+"use client";
+
+import { useRouter } from "next/navigation";
+import { useTransition } from "react";
+import GridLoader from "react-spinners/GridLoader";
+
+export default function Pagination({ pages, currentPage, currentType }) {
+  const router = useRouter();
+
+  const [isPending, startTransition] = useTransition();
+
+  // === handlePageChange function ====
+  const handlePageChange = (page) => {
+    if (page === currentPage) return;
+
+    startTransition(() => {
+      router.push(`/blogs?type=${currentType}&page=${page}`);
+    });
+  };
+
+  return (
+    <>
+      {/* ==== loading ui / spinner ==== */}
+      {isPending && (
+        <div className="fixed inset-0 z-50 flex items-center justify-center  backdrop-blur-md">
+          <GridLoader color="#4B5563" size={22} />
+        </div>
+      )}
+
+      {/* ====== main pagination ui ====== */}
+      <div className="mt-10 flex justify-center gap-4">
+        {Array.from({ length: pages }, (_, i) => i + 1).map((p) => (
+          <button
+            key={p}
+            onClick={() => handlePageChange(p)}
+            disabled={isPending}
+            className={`px-4 py-2 rounded-lg text-sm text-foreground font-medium border border-border transition-colors disabled:cursor-not-allowed
+      ${
+        p === currentPage
+          ? "bg-gray-700 text-white cursor-not-allowed"
+          : "hover:text-white hover:bg-gray-800 cursor-pointer"
+      }
+    `}
+          >
+            {p}
+          </button>
+        ))}
+      </div>
+    </>
+  );
+}
