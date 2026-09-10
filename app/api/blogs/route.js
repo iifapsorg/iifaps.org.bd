c1ae926f13cefe7f8b62fa8a25cb88913872a420
Ismail Hossain <mdismailtanvir6@gmail.com>
2026-09-10 16:41:59 +0600

    Fixing blog creation error

--- app/api/blogs/route.js
+++ app/api/blogs/route.js
@@ -3,7 +3,7 @@
 import { NextResponse } from "next/server";
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/lib/auth";
-import { revalidatePath } from "next/cache";
+import { revalidatePath, revalidateTag } from "next/cache";
 
 import { getBlogs, createBlog } from "@/services/blog.service";
 
